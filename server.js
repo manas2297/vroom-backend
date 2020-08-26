825cb250403efbde4f30315708c8a01a1bfa817f
manas2297 <yadavmanas22@gmail.com>
2020-08-26 13:09:24 +0530

    comments removed

--- server.js
+++ server.js
@@ -4,21 +4,12 @@ const http = require('http');
 const cors = require('cors');
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
 const PORT = process.env.PORT || 5000;
-// const router = require('./router');
 const routes = require('./routes');
 const { createSocketConnection } = require('./util/socket');
 const app = express();
 app.use(cors());
 app.use(express.json());
 routes(app);
-// app.use(router);
-
-
-
 const server = http.createServer(app);
 createSocketConnection(server);
-
-
-
-
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
